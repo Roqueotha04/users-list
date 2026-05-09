745e9bc834bf54c55f725d81a264e16ce01248b8
roqueotha04 <roqueotha04@gmail.com>
2026-05-09 19:53:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    fetch('https://randomuser.me/api?results=100')
+      .then(res => res.json())
+      .then(res =>{
+        setUsers(res.results);
+      })
+      .catch(err =>{
+        console.error(err)
+      })
+  }, [])
+
+  return (
+    <>
+      <h1>prueba</h1>
+      {
+        JSON.stringify(users)
+      }
+   </>
+  )
+}
+
+export default App
